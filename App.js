ba128c5a9eea1b8dd5131ad42921564e1380c6de
Ibrahim Sulaiman <ibrahim.sulaiman@jet.com>
2019-01-31 17:41:00 +0530

    Added the react-native-video pointing to my git repo

--- App.js
+++ App.js
@@ -8,35 +8,60 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Modal, Button, StyleSheet, View, Alert } from 'react-native';
 import Video from 'react-native-video';
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Video
-          source={{ uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" }}
-          controls={true}
-          style={styles.backgroundVideo}
-        />
-      </View>
-    );
-  }
+    state = {
+        isModalVisible: false
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button
+                    onPress={() => {
+                        this.setState({
+                            isModalVisible: true
+                        })
+                    }}
+                    title="Visibility"
+                    color="#841584"
+                />
+                <Video
+                    source={{ uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" }}
+                    controls={true}
+                    paused={true}
+                    style={styles.backgroundVideo}
+                />
+                {/* <Modal
+                    animationType="slide"
+                    transparent={false}
+                    supportedOrientations={['portrait', 'landscape']}
+                    visible={this.state.isModalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                    }}>
+                    <Video
+                        source={{ uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" }}
+                        controls={true}
+                        style={styles.backgroundVideo}
+                    />
+                </Modal> */}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  }
+    container: {
+        flex: 1
+    },
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    }
 });
